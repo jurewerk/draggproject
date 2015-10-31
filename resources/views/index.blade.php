eb17df0a3e7c71e9ba7b8d6aa678be0723cecccc
Jure Werk <jure.werk@gmail.com>
2015-10-31 21:51:47 +0100

    BUG - fixed bug

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,4 +34,5 @@
 <!-- JS Script Part -->
 <script type="text/javascript" src="js/jquery-1.10.2.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.10.4.custom.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
 </html>
